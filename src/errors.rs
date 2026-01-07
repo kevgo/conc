c926c378023c6113f094b311aeef2492a6304183
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-06 18:46:34 -0600

    errors.rs

--- src/errors.rs
+++ src/errors.rs
@@ -3,7 +3,7 @@ use std::io;
 /// a Result that always has a `UserError` as the error and therefore doesn't require to specify it at each call point
 pub(crate) type Result<T> = core::result::Result<T, UserError>;
 
-/// Represents errors that can occur during command execution.
+/// errors that should be printed to the user to help them use this app correctly
 #[derive(Debug)]
 pub(crate) enum UserError {
     CannotStartCommand {
@@ -17,7 +17,7 @@ impl std::fmt::Display for UserError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             UserError::CannotStartCommand { executable, error } => {
-                write!(f, "Cannot start command '{}': {}", executable, error)
+                write!(f, "Cannot start command '{executable}': {error}")
             }
             UserError::NoCommandsProvided => {
                 write!(f, "No commands provided")
@@ -25,5 +25,3 @@ impl std::fmt::Display for UserError {
         }
     }
 }
-
-impl std::error::Error for UserError {}
