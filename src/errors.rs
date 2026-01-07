a8d09dd8a644f6555c20f1b3f3553e624509a00d
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-07 16:53:03 -0600

    errors.rs

--- src/errors.rs
+++ src/errors.rs
@@ -1,8 +1,5 @@
 use crate::subshell::Call;
 
-/// a Result that always has a `UserError` as the error and therefore doesn't require to specify it at each call point
-pub(crate) type Result<T> = core::result::Result<T, UserError>;
-
 /// errors that should be printed to the user to help them use this app correctly
 #[derive(Debug, Eq, PartialEq)]
 pub enum UserError {
@@ -22,3 +19,6 @@ impl std::fmt::Display for UserError {
         }
     }
 }
+
+/// a Result that always has a `UserError` as the error and therefore doesn't require to specify it at each call point
+pub type Result<T> = core::result::Result<T, UserError>;
