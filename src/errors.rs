ab0558fcc070ab3722c2935f1cca65e4639b78bc
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-07 16:52:42 -0600

    errors.rs

--- src/errors.rs
+++ src/errors.rs
@@ -21,4 +21,4 @@ impl std::fmt::Display for UserError {
 }
 
 /// a Result that always has a `UserError` as the error and therefore doesn't require to specify it at each call point
-pub(crate) type Result<T> = core::result::Result<T, UserError>;
+pub type Result<T> = core::result::Result<T, UserError>;
