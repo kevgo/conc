f6b70be581748f95b4420a8ba3d4a4367cdfbb56
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-07 12:22:42 -0600

    subshell.rs

--- src/subshell.rs
+++ src/subshell.rs
@@ -6,6 +6,7 @@ use std::process::Command;
 pub(crate) struct Call(String);
 
 impl Call {
+    /// provides a Command instance to execute this call in a Unix shell
     #[cfg(unix)]
     pub(crate) fn command(&self) -> Command {
         let mut command = Command::new("sh");
@@ -13,6 +14,7 @@ impl Call {
         command
     }
 
+    /// provides a Command instance to execute this call in a Windows shell
     #[cfg(windows)]
     pub(crate) fn command(&self) -> Command {
         let mut command = Command::new("cmd.exe");
