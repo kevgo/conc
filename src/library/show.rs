dbbff17d0ec4288fd575e2500c7529c61ee2d44c
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-06 15:50:39 -0500

    show.rs

--- src/library/show.rs
+++ src/library/show.rs
@@ -1,20 +1,20 @@
-/// The different ways to display the output of the commands.
+/// the different ways the command output can be displayed
 #[derive(Copy, Debug, Eq, PartialEq, Clone)]
 pub enum Show {
-    /// Display all executed commands and their output.
+    /// Display the names of the executed commands and their output.
     All,
 
-    /// Display the names of all executed commands and the output of failed commands.
+    /// Display the names of the executed commands and only the output of failed commands.
     Names,
 
-    /// Display only failed commands.
+    /// Display only the names and output of failed commands.
     Failed,
 }
 
 impl Show {
     /// indicates whether to display the command name
     #[must_use]
-    pub fn display_command(self) -> bool {
+    pub(crate) fn display_command(self) -> bool {
         match self {
             Show::All | Show::Names => true,
             Show::Failed => false,
@@ -23,7 +23,7 @@ impl Show {
 
     /// indicates whether to display the output of successful commands
     #[must_use]
-    pub fn display_success(self) -> bool {
+    pub(crate) fn display_success(self) -> bool {
         match self {
             Show::All => true,
             Show::Names | Show::Failed => false,
