b4a9cd1ece5a1da97ce613e17fc8c594c252a0d5
Kevin Goslar <kevin.goslar@gmail.com>
2026-09-16 07:34:47 -0500

    executable.rs

--- src/library/domain/executable.rs
+++ src/library/domain/executable.rs
@@ -51,22 +51,22 @@ mod tests {
         #[test]
         fn raw_command() {
             let mut command = Command::new("echo");
-            command.args(vec!["one", "two three"]);
+            command.args(vec!["single", "two parts"]);
             let executable = Executable {
                 name: S("test"),
                 command,
             };
             let have = executable.command_line();
-            let want = "echo one 'two three'";
+            let want = "echo single 'two parts'";
             assert_eq!(have, want);
         }
 
         #[test]
         #[cfg(unix)]
         fn shell_command() {
-            let executable = shell_executable("echo one \"two three\"");
+            let executable = shell_executable("echo single \"two parts\"");
             let have = executable.command_line();
-            let want = "sh -c 'echo one \"two three\"'";
+            let want = "sh -c 'echo single \"two parts\"'";
             assert_eq!(have, want);
         }
     }
@@ -79,7 +79,7 @@ mod tests {
         #[test]
         fn raw_command() {
             let mut command = Command::new("echo");
-            command.args(vec!["one", "two three"]);
+            command.args(vec!["single", "two parts"]);
             let executable = Executable {
                 name: S("test"),
                 command,
@@ -99,12 +99,12 @@ Executable {{
         #[test]
         #[cfg(unix)]
         fn shell_command() {
-            let executable = shell_executable("echo one \"two three\"");
+            let executable = shell_executable("echo single \"two parts\"");
             let have = format!("{executable:?}");
             let want = format!(
                 "\
 Executable {{
-    name: echo one \"two three\"
+    name: echo single \"two parts\"
     command: {:?}
 }}",
                 executable.command
